3dbb6dda22788afbfd85047346e556073a2f19ff
Luiz Fernando Afra Brito <luizfernandoafrabrito@gmail.com>
2024-02-24 17:22:53 -0300

    simplify python version

--- Makefile
+++ Makefile
@@ -1,23 +1,25 @@
 PYTHON_EXEC := env/bin/python
-TORCH_DIR := env/lib/python3.11/site-packages/torch
+PYTHON_LIB := env/lib/python3.11/site-packages
+TORCH_DIR := $(PYTHON_LIB)/torch
+NUMPY_DIR := $(PYTHON_LIB)/numpy
 
-all: $(PYTHON_EXEC) $(TORCH_DIR)
+all: $(PYTHON_EXEC) $(TORCH_DIR) $(NUMPY_DIR)
 	$(MAKE) -C linear_regression
 
 run:
 	@PYTHON_EXEC=../$(PYTHON_EXEC) $(MAKE) -C linear_regression run
 
-$(PYTHON_EXEC): 
+$(PYTHON_EXEC):
 	python -m venv env
 
-$(TORCH_DIR): 
+$(TORCH_DIR):
 	env/bin/pip install torch
 
+$(NUMPY_DIR):
+	env/bin/pip install numpy
+
 clean:
-	$(MAKE) -C chap3 clean
+	$(MAKE) -C linear_regression clean
 	rm -rf env
 
 .PHONY: all run clean
-
-
-
--- linear_regression/python/main.py
+++ linear_regression/python/main.py
@@ -1,36 +1,59 @@
-from trainer import Trainer
-from data import SyntheticRegressionData
-from model import WeightDecayLinearRegression
-
-import time
-import math
 import torch
+import time
 
 
-def distance(a, b):
-    return math.sqrt(sum((a - b) ** 2))
+class LinearRegressionDataset(torch.utils.data.Dataset):
+    def __init__(self, w, b, n, noise=0.01):
+        noise = torch.randn(n, 1) * noise
+        self.x = torch.randn(n, len(w))
+        self.y = torch.matmul(self.x, torch.reshape(w, (-1, 1))) + b + noise
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, idx):
+        return (self.x[idx], self.y[idx])
 
 
+class LinearRegressionModel(torch.nn.Module):
+    def __init__(self, in_features):
+        super(LinearRegressionModel, self).__init__()
+        self.linear = torch.nn.Linear(in_features=in_features, out_features=1)
+
+    def forward(self, x):
+        return self.linear(x)
+
+
+torch.manual_seed(2024)
+
+weight_len = 4098
 batch_size = 64
-data = SyntheticRegressionData(
-    w=torch.tensor([2, -3.4, 4, 0.1, -5.2]),
-    b=4.2,
+train_size = batch_size * 100
+max_epochs = 100
+device = torch.device("cuda")
+
+dataset = LinearRegressionDataset(
+    w=torch.randn(weight_len),
+    b=torch.randn(1),
     noise=0.01,
-    num_train=batch_size * 100,
-    num_val=batch_size * 100,
-    batch_size=batch_size,
+    n=train_size,
 )
-
-
-trainer = Trainer(max_epochs=100)
-model = WeightDecayLinearRegression(wd=2, lr=0.01)
+model = LinearRegressionModel(in_features=weight_len)
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(params=model.parameters(), lr=0.001, weight_decay=0.1)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
 
 t0 = time.time()
-trainer.fit(model, data)
+loss = None
+
+model.to(device)
+for _ in range(max_epochs):
+    for x, y in dataloader:
+        optimizer.zero_grad()
+        loss = criterion(model(x.to(device)), y.to(device))
+        loss.backward()
+        optimizer.step()
+
 t1 = time.time()
 
-with torch.no_grad():
-    model_w, model_b = model.get_w_b()
-    print(
-        f"{t1 - t0:.6f}, {distance(data.w, model_w.reshape(data.w.shape)):.6f}, {distance(data.b, model_b):.6f}"
-    )
+print(f"{t1 - t0:.6f}, {loss:.6f}")
