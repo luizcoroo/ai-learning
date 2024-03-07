936b7dd0e37ace58c0f4bef68b56fa1bdf75c8a6
Luiz Fernando Afra Brito <luiz@finspace.ai>
2024-03-07 12:40:13 -0300

    going home sweet home

--- linear_classification/c_cpu2/include/tensor.h
+++ linear_classification/c_cpu2/include/tensor.h
@@ -1,43 +1,88 @@
+#include <stdint.h>
+
 #define MAX_DIMS 8
 
 typedef struct {
-  float *data;
   int sizes[MAX_DIMS];
   int strides[MAX_DIMS];
   int data_len;
   int rank;
-} TensorViewF32;
+} DataDesc;
 
-typedef struct {
-  unsigned char *data;
-  int sizes[MAX_DIMS];
-  int strides[MAX_DIMS];
-  int data_len;
-  int rank;
-} TensorViewU8;
+#define DECLARE_TENSOR_STRUCT(TENSOR_SUFFIX, DATA_TYPE)                        \
+  typedef struct {                                                             \
+    DATA_TYPE *data;                                                           \
+    DataDesc desc;                                                             \
+  } TensorView##TENSOR_SUFFIX;
 
-TensorViewF32 tensor_at(TensorViewF32 view, const int *indexes, int rank);
+#define DECLARE_TENSOR_SIMPLE_OPS(TENSOR_SUFFIX, OP_SUFFIX)                    \
+  TensorView##TENSOR_SUFFIX tensor_at_##OP_SUFFIX(                             \
+      TensorView##TENSOR_SUFFIX view, const int *indexes, int rank);           \
+  void tensor_exp_##OP_SUFFIX(TensorView##TENSOR_SUFFIX a);                    \
+  void tensor_log_##OP_SUFFIX(TensorView##TENSOR_SUFFIX a);                    \
+  void tensor_neg_##OP_SUFFIX(TensorView##TENSOR_SUFFIX a);                    \
+  void tensor_add_##OP_SUFFIX(TensorView##TENSOR_SUFFIX a,                     \
+                              TensorView##TENSOR_SUFFIX b);                    \
+  void tensor_sub_##OP_SUFFIX(TensorView##TENSOR_SUFFIX a,                     \
+                              TensorView##TENSOR_SUFFIX b);                    \
+  void tensor_mul_##OP_SUFFIX(TensorView##TENSOR_SUFFIX a,                     \
+                              TensorView##TENSOR_SUFFIX b);                    \
+  void tensor_div_##OP_SUFFIX(TensorView##TENSOR_SUFFIX a,                     \
+                              TensorView##TENSOR_SUFFIX b);                    \
+  void tensor_describe_##OP_SUFFIX(TensorView##TENSOR_SUFFIX view);
 
-void tensor_align_to(TensorViewF32 *a, int rank);
-void tensor_unalign(TensorViewF32 *a);
-void tensor_broadcast_to(TensorViewF32 *a, const int *sizes, int rank);
-void tensor_unbroadcast(TensorViewF32 *a);
-void tensor_squeeze_all(TensorViewF32 *a);
+#define DECLARE_TENSOR_DATA_DEPENDENT_OPS(TENSOR_SUFFIX, DATA_TYPE, OP_SUFFIX) \
+  TensorView##TENSOR_SUFFIX tensor_view_##OP_SUFFIX(                           \
+      DATA_TYPE *data, const int *sizes, int rank);                            \
+  TensorView##TENSOR_SUFFIX tensor_sum_##OP_SUFFIX(                            \
+      DATA_TYPE *a_data, TensorView##TENSOR_SUFFIX b, const int *dims, int n); \
+  TensorView##TENSOR_SUFFIX tensor_max_##OP_SUFFIX(                            \
+      DATA_TYPE *a_data, TensorView##TENSOR_SUFFIX b, const int *dims, int n); \
+  TensorView##TENSOR_SUFFIX tensor_min_##OP_SUFFIX(                            \
+      DATA_TYPE *a_data, TensorView##TENSOR_SUFFIX b, const int *dims, int n);
 
-TensorViewF32 tensor_view_f32(float *data, const int *sizes, int rank);
-TensorViewU8 tensor_view_u8(unsigned char *data, const int *sizes, int rank);
+#define DECLARE_TENSOR_MATMUL_OP(DATA_TYPE1, TENSOR_SUFFIX2, TENSOR_SUFFIX3,   \
+                                 OP_SUFFIX)                                    \
+  TensorView##TENSOR_SUFFIX3 tensor_matmul_##OP_SUFFIX(                        \
+      DATA_TYPE1 *a_data, TensorView##TENSOR_SUFFIX2 b,                        \
+      TensorView##TENSOR_SUFFIX3 c);
 
-void tensor_exp_f32(TensorViewF32 a);
-void tensor_log_f32(TensorViewF32 a);
-void tensor_neg_f32(TensorViewF32 a);
-void tensor_add_f32(TensorViewF32 a, TensorViewF32 b);
-void tensor_sub_f32(TensorViewF32 a, TensorViewF32 b);
-void tensor_mul_f32(TensorViewF32 a, TensorViewF32 b);
-void tensor_div_f32(TensorViewF32 a, TensorViewF32 b);
-TensorViewF32 tensor_sum_f32(float *a_data, TensorViewF32 b, const int *dims, int n);
-TensorViewF32 tensor_max_f32(float *a_data, TensorViewF32 b, const int *dims, int n);
-TensorViewF32 tensor_min_f32(float *a_data, TensorViewF32 b, const int *dims, int n);
-TensorViewF32 tensor_matmul_f32xf32(float *a_data, TensorViewF32 b, TensorViewF32 c);
-TensorViewF32 tensor_matmul_u8xf32(float *a_data, TensorViewU8 b, TensorViewF32 c);
+#define DECLARE_TENSOR_BASIC(TENSOR_SUFFIX, DATA_TYPE, OP_SUFFIX)              \
+  DECLARE_TENSOR_STRUCT(TENSOR_SUFFIX, DATA_TYPE)                              \
+  DECLARE_TENSOR_SIMPLE_OPS(TENSOR_SUFFIX, OP_SUFFIX)                          \
+  DECLARE_TENSOR_DATA_DEPENDENT_OPS(TENSOR_SUFFIX, DATA_TYPE, OP_SUFFIX)
 
-void tensor_describe(TensorViewF32 view);
+DECLARE_TENSOR_BASIC(U8, uint8_t, u8);
+DECLARE_TENSOR_BASIC(U16, uint16_t, u16);
+DECLARE_TENSOR_BASIC(U32, uint32_t, u32);
+DECLARE_TENSOR_BASIC(U64, uint64_t, u64);
+
+DECLARE_TENSOR_BASIC(F16, _Float16, f16);
+DECLARE_TENSOR_MATMUL_OP(_Float16, U8, F16, u8xf16);
+DECLARE_TENSOR_MATMUL_OP(_Float16, U16, F16, u16xf16);
+DECLARE_TENSOR_MATMUL_OP(_Float16, U32, F16, u32xf16);
+DECLARE_TENSOR_MATMUL_OP(_Float16, U64, F16, u64xf16);
+DECLARE_TENSOR_MATMUL_OP(_Float16, F16, F16, f16xf16);
+
+DECLARE_TENSOR_BASIC(F32, float, f32);
+DECLARE_TENSOR_MATMUL_OP(float, U8, F32, u8xf32);
+DECLARE_TENSOR_MATMUL_OP(float, U16, F32, u16xf32);
+DECLARE_TENSOR_MATMUL_OP(float, U32, F32, u32xf32);
+DECLARE_TENSOR_MATMUL_OP(float, U64, F32, u64xf32);
+DECLARE_TENSOR_MATMUL_OP(float, F16, F32, f16xf32);
+DECLARE_TENSOR_MATMUL_OP(float, F32, F32, f32xf32);
+
+DECLARE_TENSOR_BASIC(F64, double, f64);
+DECLARE_TENSOR_MATMUL_OP(double, U8, F64, u8xf64);
+DECLARE_TENSOR_MATMUL_OP(double, U16, F64, u16xf64);
+DECLARE_TENSOR_MATMUL_OP(double, U32, F64, u32xf64);
+DECLARE_TENSOR_MATMUL_OP(double, U64, F64, u64xf64);
+DECLARE_TENSOR_MATMUL_OP(double, F16, F64, f16xf64);
+DECLARE_TENSOR_MATMUL_OP(double, F32, F64, f32xf64);
+DECLARE_TENSOR_MATMUL_OP(double, F64, F64, f64xf64);
+
+void tensor_align_to(DataDesc *d, int rank);
+void tensor_unalign(DataDesc *a);
+void tensor_broadcast_to(DataDesc *a, const int *sizes, int rank);
+void tensor_unbroadcast(DataDesc *a);
+void tensor_squeeze_all(DataDesc *a);
--- linear_classification/c_cpu2/src/tensor.c
+++ linear_classification/c_cpu2/src/tensor.c
@@ -22,173 +22,6 @@ TensorViewF32 tensor_at(TensorViewF32 view, const int *indexes, int rank) {
   return new_view;
 }
 
-void tensor_align_to(TensorViewF32 *a, int rank) {
-  int rank_diff = rank - a->rank;
-  for (int i = a->rank; i > 0; --i) {
-    a->sizes[i - 1 + rank_diff] = a->sizes[i - 1];
-    a->strides[i - 1 + rank_diff] = a->strides[i - 1];
-  }
-
-  for (int i = 0; i < rank_diff; i++) {
-    a->sizes[i] = 1;
-    a->strides[i] = a->data_len;
-  }
-
-  a->rank = rank;
-}
-
-void tensor_unalign(TensorViewF32 *a) {
-  int rank_diff = 0;
-  while (rank_diff < a->rank && a->sizes[rank_diff] == 1)
-    rank_diff += 1;
-
-  for (int i = rank_diff; i < a->rank; ++i) {
-    a->sizes[i - rank_diff] = a->sizes[i];
-    a->strides[i - rank_diff] = a->strides[i];
-  }
-
-  a->rank -= rank_diff;
-}
-
-void tensor_squeeze_all(TensorViewF32 *a) {
-  int i = 0, j = 0;
-  for (; j < a->rank; i++, j++) {
-    if (a->sizes[j] == 1)
-      while (j < a->rank && a->sizes[j] == 1)
-        j++;
-
-    a->sizes[i] = a->sizes[j];
-    a->strides[i] = a->strides[j];
-  }
-  a->rank -= j - i;
-}
-
-void tensor_unbroadcast(TensorViewF32 *a) {
-  for (int i = 0; i < a->rank; ++i) {
-    if (a->strides[i] == 0) {
-      a->strides[i] = 1;
-      a->sizes[i] = 1;
-    }
-  }
-}
-
-void tensor_broadcast_to(TensorViewF32 *a, const int *sizes, int rank) {
-  for (int i = 0; i < rank; i++) {
-    if (a->sizes[i] == 1 && sizes[i] > 1) {
-      a->sizes[i] = sizes[i];
-      a->strides[i] = 0;
-    }
-  }
-}
-
-int incr_pos_at_dim(int *pos, int *idxs, const int *sizes, const int *strides,
-                    int dim);
-
-#define TENSOR_VIEW(NAME, T1, S1)                                              \
-  T1 tensor_view_##NAME(S1 *data, const int *sizes, int rank) {                \
-    T1 view = {                                                                \
-        .data = data,                                                          \
-        .rank = rank,                                                          \
-    };                                                                         \
-    memcpy(view.sizes, sizes, sizeof(int) * rank);                             \
-    int stride = 1;                                                            \
-    for (int i = rank; i > 0; i--) {                                           \
-      view.strides[i - 1] = stride;                                            \
-      stride *= view.sizes[i - 1];                                             \
-    }                                                                          \
-    view.data_len = stride;                                                    \
-    return view;                                                               \
-  }
-
-#define TENSOR_UNARY(NAME, OP)                                                 \
-  void tensor_##NAME(TensorViewF32 a) {                                        \
-    for (int i = 0; i < a.data_len; i++)                                       \
-      a.data[i] = OP(a.data[i]);                                               \
-  }
-
-#define TENSOR_ELEMENTWISE(NAME, OP)                                           \
-  void tensor_##NAME(TensorViewF32 a, TensorViewF32 b) {                       \
-    tensor_align_to(&b, a.rank);                                               \
-    tensor_broadcast_to(&b, a.sizes, a.rank);                                  \
-    int pos_a = 0, idxs_a[MAX_DIMS] = {0}, pos_b = 0, idxs_b[MAX_DIMS] = {0};  \
-    do {                                                                       \
-      a.data[pos_a] = a.data[pos_a] OP b.data[pos_b];                          \
-    } while (incr_pos_at_dim(&pos_a, idxs_a, a.sizes, a.strides, a.rank) &&    \
-             incr_pos_at_dim(&pos_b, idxs_b, b.sizes, b.strides, b.rank));     \
-  }
-
-#define TENSOR_CONTRACTION(NAME, OP, INITIAL_VALUE)                            \
-  TensorViewF32 tensor_##NAME(float *a_data, TensorViewF32 b, const int *dims, \
-                              int n) {                                         \
-    int a_sizes[MAX_DIMS];                                                     \
-    memcpy(a_sizes, b.sizes, sizeof(int) * b.rank);                            \
-    for (int i = 0; i < n; i++)                                                \
-      a_sizes[dims[i]] = 1;                                                    \
-    TensorViewF32 a = tensor_view_f32(a_data, a_sizes, b.rank);                \
-    tensor_broadcast_to(&a, b.sizes, b.rank);                                  \
-    int pos_a = 0, idxs_a[MAX_DIMS] = {0}, pos_b = 0, idxs_b[MAX_DIMS] = {0};  \
-    for (int i = 0; i < a.data_len; i++)                                       \
-      a.data[i] = INITIAL_VALUE;                                               \
-    do {                                                                       \
-      a.data[pos_a] = OP(a.data[pos_a], b.data[pos_b]);                        \
-    } while (incr_pos_at_dim(&pos_a, idxs_a, a.sizes, a.strides, a.rank) &&    \
-             incr_pos_at_dim(&pos_b, idxs_b, b.sizes, b.strides, b.rank));     \
-    tensor_unbroadcast(&a);                                                    \
-    return a;                                                                  \
-  }
-
-#define TENSOR_MATMUL(NAME, T1, PLUS_OP, MUL_OP, INITIAL_VALUE)                \
-  TensorViewF32 tensor_##NAME(float *a_data, T1 b, TensorViewF32 c) {          \
-    int dim = b.rank - 2;                                                      \
-    tensor_align_to(&c, b.rank);                                               \
-    tensor_broadcast_to(&c, b.sizes, dim);                                     \
-    int N = b.sizes[dim], K = c.sizes[dim], M = c.sizes[dim + 1];              \
-    assert(b.sizes[dim + 1] == K);                                             \
-    int a_sizes[MAX_DIMS];                                                     \
-    memcpy(a_sizes, b.sizes, sizeof(int) * dim);                               \
-    a_sizes[dim] = N;                                                          \
-    a_sizes[dim + 1] = M;                                                      \
-    TensorViewF32 a = tensor_view_f32(a_data, a_sizes, b.rank);                \
-    int pos_a = 0, idxs_a[MAX_DIMS] = {0};                                     \
-    int pos_b = 0, idxs_b[MAX_DIMS] = {0};                                     \
-    int pos_c = 0, idxs_c[MAX_DIMS] = {0};                                     \
-    do {                                                                       \
-      for (int i = 0; i < N; i++) {                                            \
-        int a_i = i * a.strides[dim], b_i = i * b.strides[dim];                \
-        for (int j = 0; j < M; j++) {                                          \
-          int a_j = j * a.strides[dim + 1], c_j = j * c.strides[dim + 1];      \
-          auto res = &a.data[pos_a + a_i + a_j];                               \
-          *res = INITIAL_VALUE;                                                \
-          for (int k = 0; k < K; k++) {                                        \
-            int b_k = k * b.strides[dim + 1], c_k = k * c.strides[dim];        \
-            *res = *res PLUS_OP b.data[pos_b + b_i + b_k] MUL_OP c             \
-                        .data[pos_c + c_k + c_j];                              \
-          }                                                                    \
-        }                                                                      \
-      }                                                                        \
-    } while (incr_pos_at_dim(&pos_a, idxs_a, a.sizes, a.strides, dim) &&       \
-             incr_pos_at_dim(&pos_b, idxs_b, b.sizes, b.strides, dim) &&       \
-             incr_pos_at_dim(&pos_c, idxs_c, c.sizes, c.strides, dim));        \
-    return a;                                                                  \
-  }
-
-float fsum(float a, float b) { return a + b; }
-
-TENSOR_VIEW(f32, TensorViewF32, float);
-TENSOR_VIEW(u8, TensorViewU8, unsigned char);
-TENSOR_UNARY(exp_f32, expf);
-TENSOR_UNARY(log_f32, logf);
-TENSOR_UNARY(neg_f32, -);
-TENSOR_ELEMENTWISE(add_f32, +);
-TENSOR_ELEMENTWISE(sub_f32, -);
-TENSOR_ELEMENTWISE(mul_f32, *);
-TENSOR_ELEMENTWISE(div_f32, /);
-TENSOR_CONTRACTION(sum_f32, fsum, 0);
-TENSOR_CONTRACTION(max_f32, fmax, -FLT_MAX);
-TENSOR_CONTRACTION(min_f32, fmin, +FLT_MAX);
-TENSOR_MATMUL(matmul_f32xf32, TensorViewF32, +, *, 0);
-TENSOR_MATMUL(matmul_u8xf32, TensorViewU8, +, *, 0);
-
 void tensor_describe(TensorViewF32 view) {
   printf("sizes: %d", view.sizes[0]);
   for (int i = 1; i < view.rank; i++)
@@ -235,18 +68,188 @@ void tensor_describe(TensorViewF32 view) {
   printf("\n");
 }
 
-int incr_pos_at_dim(int *pos, int *idxs, const int *sizes, const int *strides,
-                    int dim) {
+#define DEFINE_TENSOR_VIEW(T1, DATA_TYPE, OP_SUFFIX)                           \
+  T1 tensor_view_##OP_SUFFIX(DATA_TYPE *data, const int *sizes, int rank) {    \
+    T1 view = {                                                                \
+        .data = data,                                                          \
+        .desc.rank = rank,                                                     \
+    };                                                                         \
+    memcpy(view.desc.sizes, sizes, sizeof(int) * rank);                        \
+    int stride = 1;                                                            \
+    for (int i = rank; i > 0; i--) {                                           \
+      view.desc.strides[i - 1] = stride;                                       \
+      stride *= view.desc.sizes[i - 1];                                        \
+    }                                                                          \
+    view.desc.data_len = stride;                                               \
+    return view;                                                               \
+  }
+
+#define DEFINE_TENSOR_UNARY(OP, OP_SUFFIX)                                     \
+  void tensor_##OP_SUFFIX(TensorViewF32 a) {                                   \
+    for (int i = 0; i < a.desc.data_len; i++)                                  \
+      a.data[i] = OP(a.data[i]);                                               \
+  }
+
+#define DEFINE_TENSOR_ELEMENTWISE(OP, OP_SUFFIX)                               \
+  void tensor_##OP_SUFFIX(TensorViewF32 a, TensorViewF32 b) {                  \
+    tensor_align_to(&b, a.desc.rank);                                          \
+    tensor_broadcast_to(&b, a.desc.sizes, a.desc.rank);                        \
+    int pos_a = 0, idxs_a[MAX_DIMS] = {0}, pos_b = 0, idxs_b[MAX_DIMS] = {0};  \
+    do {                                                                       \
+      a.data[pos_a] = a.data[pos_a] OP b.data[pos_b];                          \
+    } while (incr_pos_at_dim(&pos_a, idxs_a, a.desc, a.rank) &&                \
+             incr_pos_at_dim(&pos_b, idxs_b, b.desc, b.rank));                 \
+  }
+
+#define DEFINE_TENSOR_CONTRACTION(OP, INITIAL_VALUE, OP_SUFFIX)                \
+  TensorViewF32 tensor_##OP_SUFFIX(float *a_data, TensorViewF32 b,             \
+                                   const int *dims, int n) {                   \
+    int a_sizes[MAX_DIMS];                                                     \
+    memcpy(a_sizes, b.desc.sizes, sizeof(int) * b.rank);                       \
+    for (int i = 0; i < n; i++)                                                \
+      a_sizes[dims[i]] = 1;                                                    \
+    TensorViewF32 a = tensor_view_f32(a_data, a_sizes, b.desc.rank);           \
+    tensor_broadcast_to(&a, b.desc.sizes, b.desc.rank);                        \
+    int pos_a = 0, idxs_a[MAX_DIMS] = {0}, pos_b = 0, idxs_b[MAX_DIMS] = {0};  \
+    for (int i = 0; i < a.desc.data_len; i++)                                  \
+      a.data[i] = INITIAL_VALUE;                                               \
+    do {                                                                       \
+      a.data[pos_a] = OP(a.data[pos_a], b.data[pos_b]);                        \
+    } while (incr_pos_at_dim(&pos_a, idxs_a, a.desc, a.rank) &&                \
+             incr_pos_at_dim(&pos_b, idxs_b, b.desc, b.rank));                 \
+    tensor_unbroadcast(&a);                                                    \
+    return a;                                                                  \
+  }
+
+#define DEFINE_TENSOR_MATMUL(T1, PLUS_OP, MUL_OP, INITIAL_VALUE, OP_SUFFIX)    \
+  TensorViewF32 tensor_matmul_##OP_SUFFIX(float *a_data, T1 b,                 \
+                                          TensorViewF32 c) {                   \
+    int dim = b.desc.rank - 2;                                                 \
+    tensor_align_to(&c, b.desc.rank);                                          \
+    tensor_broadcast_to(&c, b.desc.sizes, dim);                                \
+    int N = b.desc.sizes[dim], K = c.desc.sizes[dim],                          \
+        M = c.desc.sizes[dim + 1];                                             \
+    assert(b.desc.sizes[dim + 1] == K);                                        \
+    int a_sizes[MAX_DIMS];                                                     \
+    memcpy(a_sizes, b.desc.sizes, sizeof(int) * dim);                          \
+    a_sizes[dim] = N;                                                          \
+    a_sizes[dim + 1] = M;                                                      \
+    TensorViewF32 a = tensor_view_f32(a_data, a_sizes, b.desc.rank);           \
+    int pos_a = 0, idxs_a[MAX_DIMS] = {0};                                     \
+    int pos_b = 0, idxs_b[MAX_DIMS] = {0};                                     \
+    int pos_c = 0, idxs_c[MAX_DIMS] = {0};                                     \
+    do {                                                                       \
+      for (int i = 0; i < N; i++) {                                            \
+        int a_i = i * a.desc.strides[dim], b_i = i * b.desc.strides[dim];      \
+        for (int j = 0; j < M; j++) {                                          \
+          int a_j = j * a.desc.strides[dim + 1],                               \
+              c_j = j * c.desc.strides[dim + 1];                               \
+          auto res = &a.data[pos_a + a_i + a_j];                               \
+          *res = INITIAL_VALUE;                                                \
+          for (int k = 0; k < K; k++) {                                        \
+            int b_k = k * b.desc.strides[dim + 1],                             \
+                c_k = k * c.desc.strides[dim];                                 \
+            *res = *res PLUS_OP b.data[pos_b + b_i + b_k] MUL_OP c             \
+                        .data[pos_c + c_k + c_j];                              \
+          }                                                                    \
+        }                                                                      \
+      }                                                                        \
+    } while (incr_pos_at_dim(&pos_a, idxs_a, a.desc, dim) &&                   \
+             incr_pos_at_dim(&pos_b, idxs_b, b.desc, dim) &&                   \
+             incr_pos_at_dim(&pos_c, idxs_c, c.desc, dim));                    \
+    return a;                                                                  \
+  }
+
+float fsum(float a, float b) { return a + b; }
+
+
+int incr_pos_at_dim(int *pos, int *idxs, const DataDesc *desc, int dim) {
   while (dim > 0) {
-    *pos += strides[dim - 1];
+    *pos += desc->strides[dim - 1];
     idxs[dim - 1] += 1;
-    if (idxs[dim - 1] != sizes[dim - 1])
+    if (idxs[dim - 1] != desc->sizes[dim - 1])
       break;
 
-    *pos -= idxs[dim - 1] * strides[dim - 1];
+    *pos -= idxs[dim - 1] * desc->strides[dim - 1];
     idxs[dim - 1] = 0;
     dim -= 1;
   }
 
   return dim;
 }
+
+DEFINE_TENSOR_VIEW(F32, float, f32)
+DEFINE_TENSOR_DESCRIBE(F32, float, f32)
+DEFINE_TENSOR_AT(F32, float, f32)
+DEFINE_TENSOR_UNARY(expf, exp_f32)
+DEFINE_TENSOR_UNARY(logf, log_f32)
+DEFINE_TENSOR_UNARY(-, neg_f32)
+DEFINE_TENSOR_ELEMENTWISE(+, add_f32)
+DEFINE_TENSOR_ELEMENTWISE(-, sub_f32)
+DEFINE_TENSOR_ELEMENTWISE(*, mul_f32)
+DEFINE_TENSOR_ELEMENTWISE(/, div_f32)
+DEFINE_TENSOR_CONTRACTION(fsum, 0, sum_f32)
+DEFINE_TENSOR_CONTRACTION(fmax, -FLT_MAX, max_f32)
+DEFINE_TENSOR_CONTRACTION(fmin, +FLT_MAX, min_f32)
+DEFINE_TENSOR_MATMUL(F32, +, *, 0, f32xf32)
+
+
+void tensor_align_to(DataDef *a, int rank) {
+  int rank_diff = rank - a->rank;
+  for (int i = a->rank; i > 0; --i) {
+    a->sizes[i - 1 + rank_diff] = a->sizes[i - 1];
+    a->strides[i - 1 + rank_diff] = a->strides[i - 1];
+  }
+
+  for (int i = 0; i < rank_diff; i++) {
+    a->sizes[i] = 1;
+    a->strides[i] = a->data_len;
+  }
+
+  a->rank = rank;
+}
+
+void tensor_unalign(DataDef *a) {
+  int rank_diff = 0;
+  while (rank_diff < a->rank && a->sizes[rank_diff] == 1)
+    rank_diff += 1;
+
+  for (int i = rank_diff; i < a->rank; ++i) {
+    a->sizes[i - rank_diff] = a->sizes[i];
+    a->strides[i - rank_diff] = a->strides[i];
+  }
+
+  a->rank -= rank_diff;
+}
+
+void tensor_squeeze_all(DataDef *a) {
+  int i = 0, j = 0;
+  for (; j < a->rank; i++, j++) {
+    if (a->sizes[j] == 1)
+      while (j < a->rank && a->sizes[j] == 1)
+        j++;
+
+    a->sizes[i] = a->sizes[j];
+    a->strides[i] = a->strides[j];
+  }
+  a->rank -= j - i;
+}
+
+void tensor_unbroadcast(DataDef *a) {
+  for (int i = 0; i < a->rank; ++i) {
+    if (a->strides[i] == 0) {
+      a->strides[i] = 1;
+      a->sizes[i] = 1;
+    }
+  }
+}
+
+void tensor_broadcast_to(DataDef *a, const int *sizes, int rank) {
+  for (int i = 0; i < rank; i++) {
+    if (a->sizes[i] == 1 && sizes[i] > 1) {
+      a->sizes[i] = sizes[i];
+      a->strides[i] = 0;
+    }
+  }
+}
+
